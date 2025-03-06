b3244e37401b74a00f43afc6d39cbb1704b59536
Samuel Moniz <167807044+sam324sam@users.noreply.github.com>
2025-03-06 11:10:42 +0100

    Update Usuario.php
    Creacion de la verificacion de usuario

---
+++ models/Usuario.php
@@ -0,0 +1,24 @@
+<?php
+// models/Usuario.php
+require_once '../config/database.php';
+
+class Usuario {
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function verificarCredenciales($usuario, $clave) {
+        $stmt = $this->pdo->prepare("SELECT * FROM usuario WHERE email = :email");
+        $stmt->bindParam(':email', $usuario);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if (!empty($user) && $clave == $user['clave']) {
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['id'] = $user['id'];
+            return true;
+        }
+        return false;
+    }
+}
